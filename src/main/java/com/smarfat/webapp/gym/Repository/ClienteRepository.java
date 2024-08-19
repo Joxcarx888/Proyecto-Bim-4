2c5a1ac08920e77f8c690df509437ae3195c345a
Informatica <informatica@alumno.local>
2024-08-19 11:45:38 -0600

    Arreglando errores

--- src/main/java/com/smarfat/webapp/gym/Repository/ClienteRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smarfat.webapp.gym.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.smarfat.webapp.gym.model.Cliente;
-
-public interface ClienteRepository extends JpaRepository<Cliente,Long> {
-
-}
--- src/main/java/com/smarfat/webapp/gym/repository/MaquinaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.smarfat.webapp.gym.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.smarfat.webapp.gym.model.Maquina;
-
-public interface MaquinaRepository extends JpaRepository<Maquina, Long>{
-
-}
