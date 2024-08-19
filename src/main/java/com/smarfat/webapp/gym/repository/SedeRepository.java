593a05ef8145b8e5f7dd200f09bc20a4f97c431a
Informatica <informatica@alumno.local>
2024-08-19 11:48:30 -0600

    Arreglando errores

---
+++ src/main/java/com/smarfat/webapp/gym/repository/SedeRepository.java
@@ -0,0 +1,9 @@
+package com.smarfat.webapp.gym.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.smarfat.webapp.gym.model.Sede;
+
+public interface SedeRepository extends JpaRepository<Sede, Long>{
+
+}
