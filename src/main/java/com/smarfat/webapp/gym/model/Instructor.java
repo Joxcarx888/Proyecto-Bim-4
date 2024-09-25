b79630ea4dd47339d36cd4400169b28862609126
Bsoberanis <168020376+Bsoberanis@users.noreply.github.com>
2024-09-25 15:51:00 -0600

    Update Instructor.java

--- src/main/java/com/smarfat/webapp/gym/model/Instructor.java
+++ src/main/java/com/smarfat/webapp/gym/model/Instructor.java
@@ -18,40 +18,6 @@ public class Instructor {
     String nombreInstructor;
     String telefonoInstructor;
     Double sueldoInstructor;
-    String especialidadInstructor;
-
-    @Override
-    public String toString() {
-        return "Id: " + id + " | " + nombreInstructor + " " + especialidadInstructor;
-    }
-
-    public void setNombre(String text) {
-        throw new UnsupportedOperationException("Unimplemented method 'setNombre'");
-    }
-
-    public void setNivelAcceso(String selectedItem) {
-        throw new UnsupportedOperationException("Unimplemented method 'setNivelAcceso'");
-    }
-
-    public String getNombre() {
-        throw new UnsupportedOperationException("Unimplemented method 'getNombre'");
-    }
-
-    public String getNivelAcceso() {
-        throw new UnsupportedOperationException("Unimplemented method 'getNivelAcceso'");
-    }
-
-    public void setSueldo(double double1) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setSueldo'");
-    }
-
-    public double getSueldo() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getSueldo'");
-    }
-
-    
-      
+    String especialidadInstructor; 
     
 }
